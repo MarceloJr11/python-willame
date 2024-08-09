8a74c51185af524a3796dd9a5cc969fac66123bf
MarceloJr11 <juniorbenigno180@gmail.com>
2024-08-09 14:37:56 -0300

    Update 2 cap4.py

--- 2 cap4.py
+++ 2 cap4.py
@@ -5,6 +5,7 @@ qt = int(input("Digite a quantidade de termo da sua Progressão Aritmética (PA)
 r = int(input("Digite a razão da sua Progressão Aritmética (PA): "))
 
 pa = 0
-for x in range(p1, qt + 1, r):
+p = p1 + (qt - 1) * r
+for x in range(p1, p + 1, r):
     pa += x
 print("Sua Progressão aritmética(PA) é {}".format(pa))
