0723a369d15935fa5801f7a15009e90b44100706
MarceloJr11 <juniorbenigno180@gmail.com>
2024-08-09 17:05:23 -0300

    Update 3 cap4.py

--- 3 cap4.py
+++ 3 cap4.py
@@ -1,18 +1,21 @@
-#Exercício do Capítulo 4 questão 3
+##Exercício do Capítulo 4 questão 3
 
-pm = float("inf")
-menor_nome_remedio = " "
-preco_total = 0
+n = input("Digite o nome do remédio: ")
+p = float(input("Digite o preço desse remédio: "))
+sp = 0
+maisb = n
+menorp = p
+sp += p
 for x in range (1, 5):
     n = input("Digite o nome do remédio: ")
     p = float(input("Digite o preço desse remédio: "))
-    preco_total += p
+    sp += p
+    
+    if p < menorp:
+        menorp = p
+        maisb = n
+    
+print("O nome do remédio mais barato é '{}' e seu preço é {:.2f}".format(maisb, menorp))
 
-    if p < pm:
-        pm = p
-        menor_nome_remedio = n
-print("O seu menor preço de remédio é {:.2f} e seu nome é {}".format(pm, menor_nome_remedio))
-
-media = preco_total / 5
+media = sp / 5
 print("Sua média total de preço por cada remédio é {:.2f}".format(media))
-
